973effa6a0548220ff3fba4ce0661853044a1713
Guangbin Jin <gjin2@my.harrisburgu.edu>
2020-11-29 13:06:59 -0500

    first commit

---
+++ Unit_Test/strategy_test.py
@@ -0,0 +1,12 @@
+from data import DataAPI
+from backtesting import Backtesting
+from Visualization import Visualization
+import pandas as pd 
+import matplotlib.pyplot as plt
+from strategy import Strategy
+
+usr1 = Strategy(email_add='kbin5521@gmail.com', email_pwd='Hanmeiyan!880')
+
+usr1.SMA()
+
+
